5db6bc72c4021a3f5e17619b330b688861819f62
JsebasU <48026174+JsebasU@users.noreply.github.com>
2020-01-31 23:59:50 -0500

    Update Assets
    Meteorito

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     public enum Desatres { Meterito, Volcan, Radiacion, Pandemia, Incendios };
-    Desatres desatres;
+    public GameObject meteorito;
+    public GameObject volcan;
+    public GameObject radiacion;
+    public GameObject incendio;
+    Desatres desatres = Desatres.Incendios;
     float timer;
+    bool Control;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,19 +22,43 @@ public class Spawn : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= null)
+        if (timer >= 5 && !Control)
         {
-
+            desatres = Desatres.Meterito;
+            cases();
+            timer = 0;
+            Control = true;
         }
     }
     void cases()
     {
+        
         switch (desatres)
         {
             case Desatres.Meterito:
             {
+                float Point = GetComponent<SphereCollider>().radius;
 
-                break;
+                    float usedRadius = 0;
+
+                    float x = Random.Range(-Point, Point);
+                    usedRadius += Mathf.Abs(x);
+                    float y = Random.Range(-(Point-usedRadius), Point - usedRadius);
+                    usedRadius += Mathf.Abs(y);
+                    int dir = Random.Range(0, 2);
+                    float z;
+                    if(dir == 0)
+                    {
+                        z = -(Point - usedRadius);
+                    }
+                    else
+                    {
+                        z = Point - usedRadius;
+                    }
+                    GameObject met = Instantiate(meteorito, new Vector3(x*(Random.Range(10,30)),y * (Random.Range(10, 30)), z * (Random.Range(10, 30))), Quaternion.identity);
+                    met.transform.SetParent(gameObject.transform);
+
+                    break;
             }
             case Desatres.Incendios:
             {
---
+++ Assets/meteoro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteoro : MonoBehaviour
+{
+    Rigidbody rb;
+    public float velocity = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.LookAt(Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.forward * velocity; 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // -Poblacion
+        gameObject.SetActive(false);
+    }
+}
