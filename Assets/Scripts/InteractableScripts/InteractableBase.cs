0cc9edc6618818b742dd5ad4ec627092297dec1a
Julio Cesar Muñoz Bedoya <38479586+JulioCesarM@users.noreply.github.com>
2020-02-01 14:07:00 -0500

    Update Base interactables

---
+++ Assets/Scripts/InteractableScripts/InteractableBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBase : MonoBehaviour
+{
+    public virtual void OnStartInteraction()
+    {
+        
+    }
+
+    public virtual void OnCancelInteraction()
+    {
+
+    }
+}
---
+++ Assets/Scripts/InteractableScripts/InteractableHold.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InteractableHold : InteractableBase
+{
+
+    [SerializeField] float secondsToClear = 0;
+    [SerializeField] Slider temporizador;
+
+    void Awake()
+    {
+        secondsToClear = Random.Range(2f, 10f);
+        temporizador.value = 0;
+        temporizador.maxValue = secondsToClear;
+    }
+
+    public override void OnStartInteraction()
+    {
+        StartCoroutine(SimpleCompleteEvent());
+    }
+
+    IEnumerator SimpleCompleteEvent()
+    {
+        float value = temporizador.value;
+        while (value < secondsToClear)
+        {
+            value += Time.deltaTime;
+            temporizador.value = value;
+            yield return null;
+        }
+        yield return null;
+        gameObject.SetActive(false);
+    }
+
+    public override void OnCancelInteraction()
+    {
+        StopAllCoroutines();
+    }
+
+}
