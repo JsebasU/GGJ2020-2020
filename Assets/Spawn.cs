487ace677d52ae8a39be709bd5f21c44786894f8
JsebasU <48026174+JsebasU@users.noreply.github.com>
2020-01-31 21:29:19 -0500

    Add Base Template

---
+++ Assets/Spawn.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public enum Desatres { Meterito, Volcan, Radiacion, Pandemia, Incendios };
+    Desatres desatres;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= null)
+        {
+
+        }
+    }
+    void cases()
+    {
+        switch (desatres)
+        {
+            case Desatres.Meterito:
+            {
+
+                break;
+            }
+            case Desatres.Incendios:
+            {
+                    break;
+            }
+            case Desatres.Pandemia:
+            {
+                    break;
+            }
+            case Desatres.Volcan:
+            {
+                    break;
+            }
+            case Desatres.Radiacion:
+            {
+                    break;
+            }
+        }
+    }
+  
+    
+        
+     
+}
