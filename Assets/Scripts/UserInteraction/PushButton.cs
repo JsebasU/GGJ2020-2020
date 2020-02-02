38bcbfccad351ce5cd0186808aea3206e7c08339
Andrés González <andresf1991@msn.com>
2020-02-02 01:54:19 -0500

    Botones, espacio
    - Botones, espacio

---
+++ Assets/Scripts/UserInteraction/PushButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PushButton : MonoBehaviour
+{
+    public Sprite[] buttonSprite;
+    public Image buttonImage;
+    
+    public void ChangeSprite()
+    {
+        this.buttonImage.sprite = (this.buttonImage.sprite == buttonSprite[0] ? buttonSprite[1] : buttonSprite[0]);
+    }
+}
